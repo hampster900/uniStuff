b8a758d3e8bba3c57e7bbf77a09766627e86ea53
Dan <dan@pintsize.it>
2013-11-06 20:59:38 +0100

    Setting up budget folder

---
+++ budget/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Budget::Application.config.secret_key_base = '130037bbd210e6ef150d89ec39320a9739f50482fef7d9ffee46958cacc02f4877e8eb9c3c0cd350ce7406c783b638ed07756230446ce1a8542d8268119d1bbe'
